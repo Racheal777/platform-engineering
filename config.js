cf0f64e7b1e5b5d8af54adad85b53e62a266fed3
Racheal777 <kuranchieracheal@gmail.com>
2026-07-24 16:16:57 +0000

    test commit with fake key

--- config.js
+++ config.js
@@ -2,3 +2,9 @@ const awsConfig = {
     accessKeyId: "AKIAFAKEEXAMPLE12345",
     secretAccessKey: "fakeSecretKeyForTestingOnly1234567890"
 };
+
+
+const gitConfig = {
+    accessKeyId: "AKIAFAKEEXAMPLE12345",
+    secretAccessKey: "fakeSecretKeyForTestingOnly1234567890"
+};
